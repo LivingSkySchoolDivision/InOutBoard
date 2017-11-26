427bd2339355aff203bed18624b48c6fc7a8d81e
Mark Strendin <mark.strendin@gmail.com>
2017-11-26 14:13:10 -0600

    Initial commit

---
+++ LSKYInOut/Model/TrackedUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LSKYInOut
+{
+    public class TrackedUser
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NavisionEmployeIDNumber { get; set; }
+        public string LDAPUserName { get; set; }
+        public bool IsHidden { get; set; }
+        List<Group> Groups { get; set; }
+    }
+}
---
+++ LSKYInOut/Model/UserStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LSKYInOut
+{
+    public class UserStatus
+    {
+        public int UserID { get; set; }
+        public int StatusID { get; set; }
+        public string Color { get; set; }
+    }
+}
